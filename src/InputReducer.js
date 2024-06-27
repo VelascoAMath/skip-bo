0c8b3b7e59f703eec447296970550ad82c86fb64
Alfredo Velasco <avelasco@mcelroy.com>
2024-06-27 13:08:37 -0500

    Added a dispatch method to delete keys from the state

--- src/InputReducer.js
+++ src/InputReducer.js
@@ -9,6 +9,19 @@ export default function inputReducer(state, action) {
 
             return {...state, [action.key]: action.value};
         }
+        case 'delete-key': {
+
+            let newState = {};
+
+            for(const [key, value] of Object.entries(state)){
+                if(key === action.key){
+                    continue;
+                }
+                newState[key] = value;
+            }
+
+            return newState;
+        }
         default: {
             console.log(state);
             console.log(action);
